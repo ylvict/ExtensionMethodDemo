ed224b3c3ffe927adeb09eacca3be95640a45052
ylvict <ylvict@gmail.com>
2014-12-31 01:01:30 -0800

    Add Project source

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodDemo
+{
+    static class MyClass
+    {
+        public static int ToInt(this string str)
+        {
+            return Convert.ToInt32(str);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "1";
+            var i = str.ToInt();
+            Console.WriteLine(i);
+        }
+    }
+}
